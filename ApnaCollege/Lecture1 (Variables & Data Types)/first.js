8624559c3a49e6954ef125a6a079df5f75c48667
imvickykumar999 <imvickykumar999@gmail.com>
2023-11-26 12:02:18 +0530

    info to table
    https://chat.openai.com/share/10c48a1b-bee8-4bb8-8f2d-c99996260b74

--- ApnaCollege/Lecture1 (Variables & Data Types)/first.js
+++ ApnaCollege/Lecture1 (Variables & Data Types)/first.js
@@ -1,2 +1,38 @@
 
+// https://stackoverflow.com/a/77550630/11493297
+// Live Server: http://127.0.0.1:5500/
+
 console.log("Apna College");
+console.log("I love JS");
+console.log("Vicky K.");
+
+// no need to define datatype.
+
+fullName = "Tony Stark"; // string
+console.log(fullName);
+
+age = 24; // int
+console.log(age);
+
+price = 99.99; // float
+console.log(price);
+
+x = null; // known and empty
+console.log(x);
+
+y = undefined; // unknown
+console.log(y);
+
+isFollow = false; // boolean
+console.log(isFollow); 
+
+// variable is dynamically typed.
+
+a = 'hello';
+a = 3.14;
+// a = 24;
+console.log(a);
+
+// var : Variable can be re-declared & updated. A global scope variable.
+// let : Variable cannot be re-declared but can be updated. A block scope variable.
+// const : Variable cannot be re-declared or updated. A block scope variable.
